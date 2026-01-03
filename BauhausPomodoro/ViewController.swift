3128b7d3368d97e9a265a914923cac95306146b7
aydgnme <mertaydogn0@gmail.com>
2026-01-03 13:19:08 +0200

    Initial Commit

---
+++ BauhausPomodoro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BauhausPomodoro
+//
+//  Created by Mert Aydogan on 03.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
